b4470887fb076201f1728d261a857f533ecc7b5e
MdAhnafArshad <ahnafarshad.me@gmail.com>
2023-05-06 20:24:38 +0600

    added all

--- index.js
+++ index.js
@@ -4,19 +4,45 @@ const cors = require('cors');
 const port = 5000;
 
 
-const categories = require('./data/categories.json')
+const categories = require('./data/categories.json');
+const news = require('./data/news.json');
 
 
 app.use(cors());
 
-app.get('/categories', (req, res)=>{
-    res.send(categories)
+app.get('/categories', (req, res) => {
+    res.send(categories);
 })
 
-app.get('/', (req, res)=> {
+
+// all news hear
+app.get('/news', (req, res) => {
+    res.send(news);
+})
+
+//specifics news
+app.get('/news/:id', (req, res) => {
+
+    const id = req.params.id;
+    const selectedNews = news.find(n => n._id === id);
+    res.send(selectedNews);
+})
+
+app.get('/category/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    
+    if (id === 0) {
+        res.send(news);
+    } else {
+        const category = news.filter(n => parseInt(n.category_id) === id);
+        res.send(category);
+    }
+})
+
+app.get('/', (req, res) => {
     res.send('Dragon News');
 })
 
-app.listen(port, ()=>{
+app.listen(port, () => {
     console.log(`listening on port is ${port}`);
 })
